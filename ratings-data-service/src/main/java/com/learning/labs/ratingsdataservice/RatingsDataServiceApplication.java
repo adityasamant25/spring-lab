6463c6970ce15d96769c662669f9065ab7a0f415
ADSAMANT <adityasamant@gmail.com>
2020-09-05 18:04:14 +0530

    Skeleton commit

---
+++ ratings-data-service/src/main/java/com/learning/labs/ratingsdataservice/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.labs.ratingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
