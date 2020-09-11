3bebfdaedf3e957798ebc879faba6c30c1266730
ADSAMANT <adityasamant@gmail.com>
2020-09-11 16:41:21 +0530

    Fixed the Hystrix dashboard after enabling TLS

--- movie-catalog-service/src/main/java/com/learning/labs/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/learning/labs/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,25 +4,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.learning.labs.moviecatalogservice.models.AuthenticationRequest;
-import com.learning.labs.moviecatalogservice.models.AuthenticationResponse;
 import com.learning.labs.moviecatalogservice.models.CatalogItem;
 import com.learning.labs.moviecatalogservice.models.UserRating;
 import com.learning.labs.moviecatalogservice.services.MovieInfo;
-import com.learning.labs.moviecatalogservice.services.MyUserDetailsService;
 import com.learning.labs.moviecatalogservice.services.UserRatingInfo;
-import com.learning.labs.moviecatalogservice.utils.JwtUtil;
 
 @RestController
 @RequestMapping("/catalog")
@@ -34,15 +24,6 @@ public class MovieCatalogResource {
 	@Autowired
 	UserRatingInfo userRatingInfo;
 
-	@Autowired
-	AuthenticationManager authenticationManager;
-
-	@Autowired
-	MyUserDetailsService userDetailsService;
-	
-	@Autowired
-	JwtUtil jwtUtil;
-
 	@RequestMapping(method = RequestMethod.GET, value = "/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -51,23 +32,6 @@ public class MovieCatalogResource {
 		return userRating.getUserRatings().stream().map(rating -> movieInfo.getCatalogItem(rating))
 				.collect(Collectors.toList());
 	}
-
-	@RequestMapping(method = RequestMethod.POST, value = "/authenticate")
-	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
-			throws Exception {
-		try {
-			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
-		} catch (BadCredentialsException e) {
-			throw new Exception("Incorrect username or password");
-		}
-		UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-		
-		String jwt = jwtUtil.generateToken(userDetails);
-		
-		return ResponseEntity.ok(new AuthenticationResponse(jwt));
-	}
-
 }
 
 //For reactive programming
--- movie-catalog-service/src/main/java/com/learning/labs/moviecatalogservice/security/SecurityConfigurer.java
+++ movie-catalog-service/src/main/java/com/learning/labs/moviecatalogservice/security/SecurityConfigurer.java
@@ -32,8 +32,8 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll().anyRequest()
-				.authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.csrf().disable().authorizeRequests().antMatchers("/catalog/**").authenticated().anyRequest()
+				.permitAll().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
